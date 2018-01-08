31584045f13cd1e14e315fb71458947c1a59c598
Shawn Hayes <defenseor@gmail.com>
2018-01-08 08:17:27 -0500

    Add dependencies, started outlining logic

---
+++ Appointo/Appointo.Library/Models/Address.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Appointo.Entity;
+
+namespace Appointo.Library.Models
+{
+  public class Address
+  {
+    public string AddressLine1 { get; set; }
+    public string AddressLine2 { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public int ZipCode = 0;
+  }
+}
---
+++ Appointo/Appointo.Library/Models/LibHelper.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Appointo.Entity;
+
+namespace Appointo.Library.Models
+{
+  public class LibHelper
+  {
+    private LibHelper()
+    {
+      int i = 1;
+    }
+  }
+}
