89121b2bddf052bc70538d8dae06e8c648109738
Shawn Hayes <defenseor@gmail.com>
2018-01-10 12:03:39 -0500

    Revert "generated some EF stuff"
    This reverts commit 62fba221bb0169776c42610249dcdab4daa28c50.

--- Appointo/Appointo.Entity/DataEntityModel.Designer.cs
+++ Appointo/Appointo.Entity/DataEntityModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Documents\Revature\Appointo\Appointo\Appointo.Entity\DataEntityModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Bak\Documents\revature\Appointo\Appointo\Appointo\Appointo.Entity\DataEntityModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
