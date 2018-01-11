2b9372c0f940cb9925666255d1de5791766f1a0b
geoff cawley <geoffcawley@gmail.com>
2018-01-11 09:09:57 -0500

    lot of refactoring to make mvc start to work plenty of redundant stuff here; I'm too afraid to delete stuff on a shared repository

---
+++ Appointo/Appointo.Data/Models/Location.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appointo.Data.Models
+{
+	public partial class Location
+	{
+		public Location()
+		{
+			this.Doctors = new HashSet<Doctor>();
+		}
+
+		public int Id { get; set; }
+
+		public virtual ICollection<Doctor> Doctors { get; set; }
+	}
+}
---
+++ Appointo/Appointo.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Appointo.Web.MvcApplication" Language="C#" %>
