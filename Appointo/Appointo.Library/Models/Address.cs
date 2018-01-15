0ac78f0c4f90e64e13040f384c537ed52453b30b
Shawn Hayes <defenseor@gmail.com>
2018-01-14 22:21:23 -0500

    comment out the call to entity

--- Appointo/Appointo.Library/Models/Address.cs
+++ Appointo/Appointo.Library/Models/Address.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
-using Appointo.Entity;
+//using Appointo.Entity;
 
 namespace Appointo.Library.Models
 {
