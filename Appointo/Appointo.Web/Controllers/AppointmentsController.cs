abc0918ca01702af1e756e5e5d1566b403e018e8
Shawn Hayes <defenseor@gmail.com>
2018-01-17 13:07:49 -0500

    remove drptaptid col

--- Appointo/Appointo.Web/Controllers/AppointmentsController.cs
+++ Appointo/Appointo.Web/Controllers/AppointmentsController.cs
@@ -46,7 +46,7 @@ namespace Appointo.Web.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Date,Duration,Reason,DoctorPatientAppointmentId")] Appointment appointment)
+        public ActionResult Create([Bind(Include = "Id,Date,Duration,Reason")] Appointment appointment)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +78,7 @@ namespace Appointo.Web.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Date,Duration,Reason,DoctorPatientAppointmentId")] Appointment appointment)
+        public ActionResult Edit([Bind(Include = "Id,Date,Duration,Reason")] Appointment appointment)
         {
             if (ModelState.IsValid)
             {
