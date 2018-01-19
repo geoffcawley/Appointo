feed0829cbee61960b5d10c9773826b2be47bca1
Shawn Hayes <defenseor@gmail.com>
2018-01-19 11:53:25 -0500

    enum fix

--- Appointo/Appointo.Entity/Duration.cs
+++ Appointo/Appointo.Entity/Duration.cs
@@ -1,10 +1,12 @@
 ﻿namespace Appointo.Entity
 {
   using System;
+  using System.Runtime.Serialization;
 
   public enum Duration : int
   {
     Hour = 60,
+    [EnumMember(Value = "Half Hour")]
     HalfHour = 30
   }
 }
