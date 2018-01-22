08a97ea1192727e53d7b13ffeb0f6a822adba7b3
geoff cawley <geoffcawley@gmail.com>
2018-01-22 11:14:28 -0500

    back to Jenkins I guess

--- AppointoWebApp/appointowebapp/src/app/rooms/rooms.service.ts
+++ AppointoWebApp/appointowebapp/src/app/rooms/rooms.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from "@angular/http";
+import { Http, RequestOptions } from "@angular/http";
 import { HttpModule } from '@angular/http/src/http_module';
 import { HttpClient } from '@angular/common/http';
 
@@ -17,9 +17,12 @@ export class RoomsService {
 
 	addRoom(roomNumber: number) {
 		const body = {RoomNumber: roomNumber};
+		let  myheaders = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: myheaders, method: 'post' });
 		this.request.post(
 			'http://localhost/appointoservice/api/rooms',
-			 body
+			 body,
+			 new Headers({ 'Content-Type': 'application/json' })
 		).subscribe();
 	}
 }
